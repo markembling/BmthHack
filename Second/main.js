4159abe8bba822e6cebe5ac68d7ac399fba8e1ba
Mark Embling <contact@markembling.info>
2010-12-04 22:28:14 +0000

    Going for a space theme here..

--- Second/main.js
+++ Second/main.js
@@ -1,12 +1,15 @@
 $(function() {
 	var canvas = $("#myCanvas");
 	var context = canvas.get(0).getContext("2d");
+	var canvasWidth = canvas.width();
+	var canvasHeight = canvas.height();
 	
 	function resizeCanvas() {
 		canvas.attr("width", $(window).get(0).innerWidth);
 		canvas.attr("height", $(window).get(0).innerHeight);
 		
-		context.fillRect(0, 0, canvas.width(), canvas.height());
+		canvasWidth = canvas.width();
+		canvasHeight = canvas.height();
 	}
 	
 	resizeCanvas();
