1dbacbcb38b946014934a9072e06d920b0cb6f16
Mark Embling <contact@markembling.info>
2010-12-04 21:54:01 +0000

    Text stuff...

--- main.js
+++ main.js
@@ -16,9 +16,33 @@ $(function() {
 	context.closePath();
 	context.fill();*/
 	
-	context.fillStyle = "#BEF3FF";
+	/*context.fillStyle = "#BEF3FF";
 	context.fillRect(200, 40, 100, 100);
 	context.fillRect(40, 40, 100, 100);
 	context.fillStyle = "#9BFF8D";
-	context.fillRect(320, 40, 100, 100);
+	context.fillRect(320, 40, 100, 100);*/
+	
+	/*context.strokeStyle = "#0072AD";
+	context.strokeRect(40, 40, 100, 100);
+	context.strokeRect(180, 40, 100, 100);
+	
+	context.lineWidth = 10;
+	context.beginPath();
+	context.moveTo(40, 180);
+	context.lineTo(420, 180);
+	context.stroke();
+	
+	context.lineWidth = 20;
+	context.beginPath();
+	context.moveTo(40, 200);
+	context.lineTo(420, 200);
+	context.stroke();*/
+	
+	/*var text = "Hello World";
+	context.font = "italic 30px Calibri";
+	context.fillText(text, 40, 40);
+	// or strokeText 
+	*/
+	
+	
 });
